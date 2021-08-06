2098e08683e143bd6b853211db7625e66aeac308
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2021-08-06 12:04:26 -0400

    Added Creative Tab

--- src/main/java/com/github/sculkhoard/core/ItemRegistry.java
+++ src/main/java/com/github/sculkhoard/core/ItemRegistry.java
@@ -2,6 +2,8 @@ package com.github.sculkhoard.core;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,5 +11,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemRegistry {
     //https://www.mr-pineapple.co.uk/tutorials/items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
-    public static final RegistryObject<Item> SCULK_MATTER = ITEMS.register("sculk_matter", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> SCULK_MATTER = ITEMS.register("sculk_matter", () -> new Item(new Item.Properties().tab(Main.SCULK_GROUP)));
+
+
 }
--- src/main/java/com/github/sculkhoard/core/Main.java
+++ src/main/java/com/github/sculkhoard/core/Main.java
@@ -1,5 +1,8 @@
 package com.github.sculkhoard.core;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -8,8 +11,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 //It takes in our mod id so it knows what mod it is loading.
 @Mod(Main.MOD_ID)
 public class Main {
+
     //Here I've created a variable of our mod id so we can use it throughout our project
     public static final String MOD_ID = "sculkhoard";
+    public static final ItemGroup SCULK_GROUP = new CreativeTabGroup(MOD_ID + "_tab");
 
     //This is the instance of our class, and we register it to the ModEventBus (which I have stored in a variable).
     public Main() {
@@ -17,8 +22,22 @@ public class Main {
         bus.register(this);
         ItemRegistry.ITEMS.register(bus); //Load Items
         BlockRegistry.BLOCKS.register(bus); //Load Blocks
+
     }
 
+    //Add Creative Item Tab
+    public static class CreativeTabGroup extends ItemGroup {
+        public CreativeTabGroup(String label) {
+            super(label);
+        }
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ItemRegistry.SCULK_MATTER.get());
+        }
 
+        @Override
+        public void fill(NonNullList<ItemStack> items) {
+        }
+    }
 
 }
