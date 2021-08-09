49801260d9aab417b47c8698d1b07a4fe3c40c62
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2021-08-09 12:15:04 -0400

    Added Item Dev Wand
    This item currently has no name or item lore associated with it in-game.

--- src/main/java/com/github/sculkhoard/core/ItemRegistry.java
+++ src/main/java/com/github/sculkhoard/core/ItemRegistry.java
@@ -1,5 +1,9 @@
 package com.github.sculkhoard.core;
 
+
+import com.github.sculkhoard.common.DevWand;
+import com.github.sculkhoard.core.Main;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -11,7 +15,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemRegistry {
     //https://www.mr-pineapple.co.uk/tutorials/items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
+    
     public static final RegistryObject<Item> SCULK_MATTER = ITEMS.register("sculk_matter", () -> new Item(new Item.Properties().tab(Main.SCULK_GROUP)));
 
-
+    public static final RegistryObject<DevWand> DEV_WAND = ITEMS.register("dev_wand", 
+    		() -> new DevWand(new Item.Properties().tab(Main.SCULK_GROUP)));
 }
