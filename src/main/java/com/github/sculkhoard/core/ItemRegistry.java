ed878315029b4aa52f896c38b47030e73af88ce1
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2021-09-22 12:28:24 -0400

    Updated Dev Wand

--- src/main/java/com/github/sculkhoard/core/ItemRegistry.java
+++ src/main/java/com/github/sculkhoard/core/ItemRegistry.java
@@ -7,6 +7,7 @@ import com.github.sculkhoard.core.Main;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,5 +20,9 @@ public class ItemRegistry {
     public static final RegistryObject<Item> SCULK_MATTER = ITEMS.register("sculk_matter", () -> new Item(new Item.Properties().tab(Main.SCULK_GROUP)));
 
     public static final RegistryObject<DevWand> DEV_WAND = ITEMS.register("dev_wand", 
-    		() -> new DevWand(new Item.Properties().tab(Main.SCULK_GROUP)));
+    		() -> new DevWand(new Item.Properties()
+    				.tab(Main.SCULK_GROUP)
+    				.durability(5)
+    				.rarity(Rarity.EPIC)
+    				.fireResistant()));
 }
