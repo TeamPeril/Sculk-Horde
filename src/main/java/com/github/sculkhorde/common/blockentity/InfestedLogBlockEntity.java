680627e6b69737bae1312b268672085134cfbe24
Mike <mrpkeleven+github@gmail.com>
2023-09-08 22:37:22 -0400

    Added Infested Logs, Fixed Purity Icon

--- src/main/java/com/github/sculkhorde/common/blockentity/InfestedLogBlockEntity.java
+++ src/main/java/com/github/sculkhorde/common/blockentity/InfestedLogBlockEntity.java
@@ -7,10 +7,13 @@ import com.github.sculkhorde.core.SculkHorde;
 import com.github.sculkhorde.core.gravemind.entity_factory.EntityFactory;
 import com.github.sculkhorde.core.gravemind.entity_factory.ReinforcementRequest;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -45,7 +48,11 @@ public class InfestedLogBlockEntity extends BlockEntity implements ITagInfestedB
         {
             return;
         }
-        this.storedNormalVariant = NbtUtils.readBlockState(level.holderLookup(Registries.BLOCK), compoundNBT.getCompound(storedNormalVariantIdentifier));
+        // This insufferable piece of code gave me a fucking headache. I borrowed this from piston code. For whatever reason, it works.
+        // This is needed because level is null on load and this allows it to save data properly
+        HolderGetter<Block> holdergetter = (HolderGetter<Block>)(this.level != null ? this.level.holderLookup(Registries.BLOCK) : BuiltInRegistries.BLOCK.asLookup());
+        setNormalBlockState(NbtUtils.readBlockState(holdergetter, compoundNBT.getCompound(storedNormalVariantIdentifier)));
+        //SculkHorde.LOGGER.debug("Infested Log Loaded State: " + storedNormalVariant.toString());
     }
 
     /**
@@ -55,17 +62,20 @@ public class InfestedLogBlockEntity extends BlockEntity implements ITagInfestedB
     @Override
     public void saveAdditional(CompoundTag compoundNBT) {
 
-        compoundNBT.put(storedNormalVariantIdentifier, NbtUtils.writeBlockState(storedNormalVariant));
+        compoundNBT.put(storedNormalVariantIdentifier, NbtUtils.writeBlockState(getNormalBlockState()));
         super.saveAdditional(compoundNBT);
+        //SculkHorde.LOGGER.debug("Infested Log Saved State: " + NbtUtils.writeBlockState(storedNormalVariant));
     }
 
     @Override
     public void setNormalBlockState(BlockState blockState) {
         this.storedNormalVariant = blockState;
+        //SculkHorde.LOGGER.debug("setNormalBlockState: " + blockState.toString());
     }
 
     @Override
     public BlockState getNormalBlockState() {
+        //SculkHorde.LOGGER.debug("getNormalBlockState: " + storedNormalVariant);
         return this.storedNormalVariant;
     }
 }
