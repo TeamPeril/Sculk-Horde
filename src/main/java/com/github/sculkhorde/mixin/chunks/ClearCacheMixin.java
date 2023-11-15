8f172bb19ad2cc6bf2de4c35f45afb887ad3de86
Mike <mrpkeleven+github@gmail.com>
2023-11-15 03:11:56 -0500

    I hate mixins part 2

--- src/main/java/com/github/sculkhorde/mixin/chunks/ClearCacheMixin.java
+++ src/main/java/com/github/sculkhorde/mixin/chunks/ClearCacheMixin.java
@@ -5,9 +5,9 @@ import net.minecraft.server.level.ServerChunkCache;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-//@Mixin(ServerChunkCache.class)
+@Mixin(ServerChunkCache.class)
 public abstract class ClearCacheMixin implements IClearCache {
-    //@Shadow
+    @Shadow
     private void clearCache() {}
 
     public void publicClearCache() {
