f274eb2d8f0b02c1f0122bcd70db8673d2ba04d9
Mike <mrpkeleven+github@gmail.com>
2023-11-18 13:47:58 -0500

    Update ModMobEffects.java

--- src/main/java/com/github/sculkhorde/core/ModMobEffects.java
+++ src/main/java/com/github/sculkhorde/core/ModMobEffects.java
@@ -12,9 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModMobEffects {
 
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, SculkHorde.MOD_ID);
-
     public static final RegistryObject<SculkInfectionEffect> SCULK_INFECTION = EFFECTS.register("sculk_infected", SculkInfectionEffect::new);
-
     public static final RegistryObject<SculkLureEffect> SCULK_LURE = EFFECTS.register("sculk_lure", SculkLureEffect::new);
     public static final RegistryObject<PurityEffect> PURITY = EFFECTS.register("purity", PurityEffect::new);
     public static final RegistryObject<DiseasedCystsEffect> DISEASED_CYSTS = EFFECTS.register("diseased_cysts", DiseasedCystsEffect::new);
