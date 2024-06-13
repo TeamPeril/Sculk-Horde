0400e5807d40d9ed94b57d28af081937da3e8adc
Mike <mrpkeleven+github@gmail.com>
2024-06-12 21:14:14 -0400

    Added Ender bubble attack sound

--- src/main/java/com/github/sculkhorde/common/entity/boss/sculk_enderman/EnderBubbleAttackEntity.java
+++ src/main/java/com/github/sculkhorde/common/entity/boss/sculk_enderman/EnderBubbleAttackEntity.java
@@ -128,7 +128,7 @@ public class EnderBubbleAttackEntity extends SpecialEffectEntity implements GeoE
 
         if(!startedPlayingSFX)
         {
-            level().playSound((Player)null, this.getX(), this.getY(), this.getZ(), ModSounds.ENDER_BUBBLE_LOOP.get(), this.getSoundSource(), 1.0F, 0.8F + this.random.nextFloat() * 0.4F);
+            level().playSound((Player)null, this.getX(), this.getY(), this.getZ(), ModSounds.ENDER_BUBBLE_LOOP.get(), this.getSoundSource(), 1.0F, 0.5F);
         }
 
         pullInEntities(20);
