ca6073f9edf718ff2aed41b752881949f1179cf0
Mike <mrpkeleven+github@gmail.com>
2024-08-08 20:02:22 -0400

    Tome of Veil is recievable

--- src/main/java/com/github/sculkhorde/common/command/VesselCommand.java
+++ src/main/java/com/github/sculkhorde/common/command/VesselCommand.java
@@ -1,10 +1,7 @@
 package com.github.sculkhorde.common.command;
 
-import com.github.sculkhorde.core.ModMobEffects;
+import com.github.sculkhorde.core.ModItems;
 import com.github.sculkhorde.core.ModSavedData;
-import com.github.sculkhorde.core.SculkHorde;
-import com.github.sculkhorde.core.gravemind.entity_factory.ReinforcementRequest;
-import com.github.sculkhorde.util.BlockAlgorithms;
 import com.github.sculkhorde.util.PlayerProfileHandler;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
@@ -16,21 +13,12 @@ import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Random;
 
 public class VesselCommand implements Command<CommandSourceStack> {
 
@@ -57,14 +45,9 @@ public class VesselCommand implements Command<CommandSourceStack> {
                             return getVessels(commandStack.getSource());
                         })
                 )
-                .then(Commands.literal("toggleVeil")
+                .then(Commands.literal("receive_enlightenment")
                         .executes((commandStack) -> {
-                            return toggleVeil(commandStack.getSource());
-                        })
-                )
-                .then(Commands.literal("summon_reinforcements")
-                        .executes((commandStack) -> {
-                            return summonReinforcementsCommand(commandStack.getSource());
+                            return recieveEnlightenment(commandStack.getSource());
                         })
                 );
 
@@ -117,7 +100,7 @@ public class VesselCommand implements Command<CommandSourceStack> {
         return vessels.size();
     }
 
-    private static int toggleVeil(CommandSourceStack context) throws CommandSyntaxException {
+    private static int recieveEnlightenment(CommandSourceStack context) throws CommandSyntaxException {
 
         if(context.getPlayer() == null)
         {
@@ -129,138 +112,15 @@ public class VesselCommand implements Command<CommandSourceStack> {
 
         if(playerProfile.isVessel())
         {
-            if(playerProfile.isActiveVessel())
-            {
-                playerProfile.setActiveVessel(false);
-                context.getPlayer().removeEffect(ModMobEffects.SCULK_VESSEL.get());
-                context.sendSuccess(() -> { return Component.literal("Your powers are hidden. You now appear normal.");}, false);
-                return 1;
-            }
-
-            playerProfile.setActiveVessel(true);
-            MobEffectInstance effectInstance = new MobEffectInstance(ModMobEffects.SCULK_VESSEL.get(), Integer.MAX_VALUE);
-            context.getPlayer().addEffect(effectInstance);
-            context.sendSuccess(() -> { return Component.literal("Your powers are now active. You now appear as a vessel.");}, false);
+            context.getPlayer().getInventory().add(new ItemStack(ModItems.TOME_OF_VEIL.get()));
+            context.sendSuccess(() -> { return Component.literal("Your now poses the Tome.");}, false);
             return 1;
         }
         else
         {
-            context.sendFailure(Component.literal("You are not a vessel of the Gravemind. Cannot Toggle Veil."));
+            context.sendFailure(Component.literal("You are not Worthy"));
             return 0;
         }
     }
-
-    public static int summonReinforcementsCommand(CommandSourceStack context)
-    {
-        context.sendSuccess(()->Component.literal(
-                "Reinforcement Request Received."
-        ), false);
-
-        Vec3 reinforcementPosition = context.getPosition();
-
-        summonReinforcement(context.getLevel(), BlockPos.containing(reinforcementPosition));
-
-        return 0;
-    }
-
-    private static void summonReinforcement(ServerLevel level, BlockPos blockPos)
-    {
-        ((ServerLevel)level).sendParticles(ParticleTypes.SCULK_SOUL, blockPos.getX() + 0.5D, blockPos.getY() + 1.15D, blockPos.getZ() + 0.5D, 2, 0.2D, 0.0D, 0.2D, 0.0D);
-        ((ServerLevel)level).playSound((Player)null, blockPos, SoundEvents.SCULK_CATALYST_BLOOM, SoundSource.BLOCKS, 2.0F, 0.6F + 1.0F);
-        //Choose spawn positions
-        ArrayList<BlockPos> possibleSpawnPositions = getSpawnPositionsInCube((ServerLevel) level, blockPos, 5, 10);
-
-        BlockPos[] finalizedSpawnPositions = new BlockPos[10];
-
-        //Create MAX_SPAWNED_ENTITIES amount of Reinforcement Requests
-        for (int iterations = 0; iterations < possibleSpawnPositions.size(); iterations++)
-        {
-            finalizedSpawnPositions[iterations] = possibleSpawnPositions.get(iterations);
-        }
-
-        //If the array is empty, just spawn above block
-        if (possibleSpawnPositions.isEmpty()) {
-            finalizedSpawnPositions[0] = blockPos.above();
-        }
-
-        //Give gravemind context to our request to make more informed situations
-        ReinforcementRequest request = new ReinforcementRequest((ServerLevel) level, finalizedSpawnPositions);
-        request.sender = ReinforcementRequest.senderType.Summoner;
-        request.is_aggressor_nearby = true;
-
-        //Request reinforcement from entity factory (this request gets approved or denied by gravemind)
-        SculkHorde.entityFactory.createReinforcementRequestFromSummoner(level, blockPos, false, request);
-    }
-
-
-    /**
-     * Returns true if the block below is a sculk block,
-     * and if the two blocks above it are free.
-     * @param worldIn The World
-     * @param pos The Position to spawn the entity
-     * @return True/False
-     */
-    public static boolean isValidSpawnPosition(ServerLevel worldIn, BlockPos pos)
-    {
-        boolean isBlockBelowSolid = BlockAlgorithms.isSolid(worldIn, pos.below());
-        boolean isBaseBlockReplaceable = worldIn.getBlockState(pos).canBeReplaced(Fluids.WATER);
-        boolean isBlockAboveReplaceable = worldIn.getBlockState(pos.above()).canBeReplaced(Fluids.WATER);
-
-        return isBlockBelowSolid && isBaseBlockReplaceable && isBlockAboveReplaceable;
-
-    }
-
-    public static ArrayList<BlockPos> getSpawnPositionsInCube(ServerLevel worldIn, BlockPos origin, int length, int amountOfPositions)
-    {
-        ArrayList<BlockPos> listOfPossibleSpawns = getSpawnPositions(worldIn, origin, length);
-        ArrayList<BlockPos> finalList = new ArrayList<>();
-        Random rng = new Random();
-        for(int count = 0; count < amountOfPositions && listOfPossibleSpawns.size() > 0; count++)
-        {
-            int randomIndex = rng.nextInt(listOfPossibleSpawns.size());
-            //Get random position between 0 and size of list
-            finalList.add(listOfPossibleSpawns.get(randomIndex));
-            listOfPossibleSpawns.remove(randomIndex);
-        }
-        return finalList;
-    }
-
-    /**
-     * Finds the location of the nearest block given a BlockPos predicate.
-     * @param worldIn The world
-     * @param origin The origin of the search location
-     * @param pDistance The search distance
-     * @return The position of the block
-     */
-    public static ArrayList<BlockPos> getSpawnPositions(ServerLevel worldIn, BlockPos origin, double pDistance)
-    {
-        ArrayList<BlockPos> list = new ArrayList<>();
-
-        //Search area for block
-        for(int i = 0; (double)i <= pDistance; i = i > 0 ? -i : 1 - i)
-        {
-            for(int j = 0; (double)j < pDistance; ++j)
-            {
-                for(int k = 0; k <= j; k = k > 0 ? -k : 1 - k)
-                {
-                    for(int l = k < j && k > -j ? j : 0; l <= j; l = l > 0 ? -l : 1 - l)
-                    {
-                        //blockpos$mutable.setWithOffset(origin, k, i - 1, l);
-                        BlockPos temp = new BlockPos(origin.getX() + k, origin.getY() + i-1, origin.getZ() + l);
-
-                        //If the block is close enough and is the right blockstate
-                        if (origin.closerThan(temp, pDistance)
-                                && isValidSpawnPosition(worldIn, temp))
-                        {
-                            list.add(temp); //add position
-                        }
-                    }
-                }
-            }
-        }
-        //else return empty
-        return list;
-    }
-
 
 }
--- src/main/java/com/github/sculkhorde/common/item/TomeOfVeilItem.java
+++ src/main/java/com/github/sculkhorde/common/item/TomeOfVeilItem.java
@@ -1,12 +1,15 @@
 package com.github.sculkhorde.common.item;
 
+import com.github.sculkhorde.core.ModItems;
 import com.github.sculkhorde.core.ModMobEffects;
 import com.github.sculkhorde.core.ModSavedData;
 import com.github.sculkhorde.util.PlayerProfileHandler;
 import com.github.sculkhorde.util.TickUnits;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.Container;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
@@ -75,6 +78,9 @@ public class TomeOfVeilItem extends TomeItem implements IForgeItem {
 				profile.setActiveVessel(false);
 				player.removeEffect(ModMobEffects.SCULK_VESSEL.get());
 				player.sendSystemMessage(Component.literal("Your powers are hidden. You now appear normal."));
+
+				removeAllItems(player, ModItems.TOME_OF_REINFORCEMENT.get());
+				removeAllItems(player, ModItems.TOME_OF_SPINES.get());
 			}
 			else
 			{
@@ -82,11 +88,23 @@ public class TomeOfVeilItem extends TomeItem implements IForgeItem {
 				MobEffectInstance effectInstance = new MobEffectInstance(ModMobEffects.SCULK_VESSEL.get(), Integer.MAX_VALUE);
 				player.addEffect(effectInstance);
 				player.sendSystemMessage(Component.literal("Your powers are now active. You now appear as a vessel."));
+				player.getInventory().add(new ItemStack(ModItems.TOME_OF_REINFORCEMENT.get()));
+				player.getInventory().add(new ItemStack(ModItems.TOME_OF_SPINES.get()));
 			}
 		}
 		else
 		{
 			player.sendSystemMessage(Component.literal("You are not a vessel of the Gravemind. Cannot Toggle Veil."));
+		}
+	}
+
+	public static void removeAllItems(Player player, Item itemToRemove) {
+		Container inventory = player.getInventory();
+		for (int i = 0; i < inventory.getContainerSize(); i++) {
+			ItemStack stackInSlot = inventory.getItem(i);
+			if (stackInSlot.getItem() == itemToRemove) {
+				inventory.setItem(i, ItemStack.EMPTY);
+			}
 		}
 	}
 }
