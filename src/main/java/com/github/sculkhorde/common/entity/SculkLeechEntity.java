9c788cfd3f6245a71e76995718bf6f8dd7ae73ba
Mike <mrpkeleven+github@gmail.com>
2025-09-30 19:14:51 -0400

    Adjust infect effect duration based on difficulty
    The duration of the Sculk Mite infect effect applied by SculkLeechEntity now varies with game difficulty: 60 seconds on Easy, 40 on Normal, and 30 on Hard. This change uses DifficultyUtil to provide a more balanced gameplay experience.

--- src/main/java/com/github/sculkhorde/common/entity/SculkLeechEntity.java
+++ src/main/java/com/github/sculkhorde/common/entity/SculkLeechEntity.java
@@ -4,6 +4,7 @@ import com.github.sculkhorde.common.entity.components.TargetParameters;
 import com.github.sculkhorde.common.entity.goal.*;
 import com.github.sculkhorde.core.ModSounds;
 import com.github.sculkhorde.core.SculkHorde;
+import com.github.sculkhorde.util.DifficultyUtil;
 import com.github.sculkhorde.util.EntityAlgorithms;
 import com.github.sculkhorde.util.SquadHandler;
 import com.github.sculkhorde.util.TickUnits;
@@ -237,7 +238,18 @@ public class SculkLeechEntity extends Salmon implements GeoEntity, ISculkSmartEn
             float targetMobRemainingHealth = target.getHealth() / target.getMaxHealth();
             if(targetMobRemainingHealth <= 0.5 && !target.hasEffect(SculkMiteEntity.INFECT_EFFECT))
             {
-                EntityAlgorithms.applyEffectToTarget(target, SculkMiteEntity.INFECT_EFFECT, SculkMiteEntity.INFECT_DURATION, SculkHorde.gravemind.getPotionAmplificationBasedOnGravemindState());
+                if(DifficultyUtil.isCurrentDifficultyEasy())
+                {
+                    EntityAlgorithms.applyEffectToTarget(target, SculkMiteEntity.INFECT_EFFECT, TickUnits.convertSecondsToTicks(60), SculkHorde.gravemind.getPotionAmplificationBasedOnGravemindState());
+                }
+                else if(DifficultyUtil.isCurrentDifficultyNormal())
+                {
+                    EntityAlgorithms.applyEffectToTarget(target, SculkMiteEntity.INFECT_EFFECT, TickUnits.convertSecondsToTicks(40), SculkHorde.gravemind.getPotionAmplificationBasedOnGravemindState());
+                }
+                else if(DifficultyUtil.isCurrentDifficultyHard())
+                {
+                    EntityAlgorithms.applyEffectToTarget(target, SculkMiteEntity.INFECT_EFFECT, TickUnits.convertSecondsToTicks(30), SculkHorde.gravemind.getPotionAmplificationBasedOnGravemindState());
+                }
 
                 //Kill The Bastard
                 /**
