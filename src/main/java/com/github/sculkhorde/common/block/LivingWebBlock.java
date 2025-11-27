be41ffa81f26a5ac2442724c0fbf9acf9cfb3ce7
Mike <mrpkeleven+github@gmail.com>
2025-11-27 08:19:04 -0500

    Living Web now slows you

--- src/main/java/com/github/sculkhorde/common/block/LivingWebBlock.java
+++ src/main/java/com/github/sculkhorde/common/block/LivingWebBlock.java
@@ -1,9 +1,13 @@
 package com.github.sculkhorde.common.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SculkVeinBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public class LivingWebBlock extends SculkVeinBlock {
     public LivingWebBlock(Properties properties) {
@@ -23,5 +27,10 @@ public class LivingWebBlock extends SculkVeinBlock {
     @Override
     public boolean canBeReplaced(BlockState p_222381_, BlockPlaceContext p_222382_) {
         return true;
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        entity.makeStuckInBlock(state, new Vec3(0.75F, 1F, 0.75F));
     }
 }
