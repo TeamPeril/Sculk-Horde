f83c0153d4b667991910c2c3a393e46ea4c4156c
Mike <mrpkeleven+github@gmail.com>
2026-03-07 23:19:26 -0500

    Add tooltips and localization for Foods of Purity
    Add client-side hover tooltips to Foods of Purity items (Bread, Beef, Pork, Chicken, Baked Potato) by implementing appendHoverText that shows functionality on Shift, lore on Ctrl, or a default hint. Add necessary client imports and @OnlyIn(Dist.CLIENT) annotation. Also add localization entries (item names and two tooltip translation keys) across multiple language files (en_us, de_de, es_*, fr_*, ru_ru, tok, zh_cn) to provide names and tooltip text for these items.

--- src/main/java/com/github/sculkhorde/common/item/BakedPotatoOfPurityItem.java
+++ src/main/java/com/github/sculkhorde/common/item/BakedPotatoOfPurityItem.java
@@ -2,14 +2,24 @@ package com.github.sculkhorde.common.item;
 
 import com.github.sculkhorde.core.ModMobEffects;
 import com.github.sculkhorde.util.TickUnits;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.extensions.IForgeItem;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
 
 public class BakedPotatoOfPurityItem extends Item implements IForgeItem {
     public BakedPotatoOfPurityItem(Properties p_41383_) {
@@ -42,5 +52,21 @@ public class BakedPotatoOfPurityItem extends Item implements IForgeItem {
         return foodProperties;
     }
 
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT))
+        {
+            tooltip.add(Component.translatable("tooltip.sculkhorde.food_of_purity.functionality"));
+        }
+        else if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL))
+        {
+            tooltip.add(Component.translatable("tooltip.sculkhorde.food_of_purity.lore"));
+        }
+        else
+        {
+            tooltip.add(Component.translatable("tooltip.sculkhorde.default"));
+        }
+    }
 
 }
