28268428654be585ffdc476110ee20410a43dbfc
Mikeatron-User <mrpkeleven@gmail.com>
2026-05-14 09:40:16 -0400

    Removed soul animator and soul distrupter items.

--- src/main/java/com/github/sculkhorde/core/ModCreativeModeTab.java
+++ src/main/java/com/github/sculkhorde/core/ModCreativeModeTab.java
@@ -254,9 +254,6 @@ public class ModCreativeModeTab {
                 event.accept(ModItems.SCULK_BROODLING_SPAWN_EGG.get());
                 event.accept(ModItems.SCULK_GHAST_SPAWN_EGG.get());
 
-
-                event.accept(ModItems.SOUL_DISRUPTER.get());
-                event.accept(ModItems.SOUL_ANIMATOR.get());
                 event.accept(ModItems.ANGEL_OF_REAPING_SOUL.get());
                 event.accept(ModBlocks.GOLEM_OF_WRATH_ANIMATOR_BLOCK.get());
                 event.accept(ModBlocks.DEPLETED_GOLEM_OF_WRATH_ANIMATOR_BLOCK.get());
--- src/main/java/com/github/sculkhorde/core/ModItems.java
+++ src/main/java/com/github/sculkhorde/core/ModItems.java
@@ -293,9 +293,6 @@ public class ModItems {
 		}
 	});
 
-	public static final RegistryObject<SoulDisrupterItem> SOUL_DISRUPTER = ITEMS.register("soul_disrupter",
-			SoulDisrupterItem::new);
-
 	public static final RegistryObject<Item> FERRISCITE = ITEMS.register("ferriscite", () -> new Item(new Item.Properties()){
 		@Override
 		@OnlyIn(Dist.CLIENT)
@@ -326,25 +323,6 @@ public class ModItems {
 			else if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL))
 			{
 				tooltip.add(Component.translatable("tooltip.sculkhorde.diascite.lore"));
-			}
-			else
-			{
-				tooltip.add(Component.translatable("tooltip.sculkhorde.default"));
-			}
-		}
-	});
-
-	public static final RegistryObject<Item> SOUL_ANIMATOR = ITEMS.register("soul_animator", () -> new Item(new Item.Properties()){
-		@Override
-		@OnlyIn(Dist.CLIENT)
-		public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-			if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT))
-			{
-				tooltip.add(Component.translatable("tooltip.sculkhorde.soul_animator.functionality"));
-			}
-			else if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_CONTROL))
-			{
-				tooltip.add(Component.translatable("tooltip.sculkhorde.soul_animator.lore"));
 			}
 			else
 			{
